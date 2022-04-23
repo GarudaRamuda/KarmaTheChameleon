2976764921994890e9d0c91083ee07fb8e6efc18
nickpastoria <84762796+nickpastoria@users.noreply.github.com>
2022-04-23 16:46:53 -0700

    Added song and implemented into game

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,12 +4,18 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
+        // Sounds
+        this.load.audio('music', './assets/Song.wav');
         this.load.image('ground', './assets/ground.png');
         this.load.image('chameleon', './assets/chameleon.png');
     }
 
 
     create() {
+        // Create and play music
+        this.music = this.sound.add('music');
+        this.music.play();
+
         this.matter.world.setBounds();
         this.ground = this.matter.add.image(400, 400, 'ground', null, { restitution: 0.4, isStatic: true });
         this.p1 = new Player(this, this.matter.world, 220, 180, 'chameleon'); // do we need setOrigin?
