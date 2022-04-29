9f87723f73d6c9f5babbd3d351a3dde136ca392b
mperina11 <mperina173@gmail.com>
2022-04-29 16:27:59 -0700

    cange alpha when cursor hovers over button

--- src/scenes/Death.js
+++ src/scenes/Death.js
@@ -21,6 +21,16 @@ class Death extends Phaser.Scene {
         let button1 = this.add.image(game.config.width/2, button1Pad, 'button').setOrigin(0.5, 0.5).setScale(2);
         button1.setInteractive();
         this.add.text(game.config.width/2, button1.y + 2, 'MENU', menuConfig).setOrigin(0.5);
+
+        // Change Alpha on button
+        button1.on('pointerover', () => {
+            button1.alpha = 0.1;
+        });
+        button1.on('pointerout', () => {
+            button1.alpha = 1;
+        });
+
+        // Go to Menu Scene
         button1.on('pointerdown', () => {
             this.scene.start('menu');
         });
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -21,14 +21,29 @@ class Menu extends Phaser.Scene {
         let button1 = this.add.image(game.config.width/2, button1Pad, 'button').setOrigin(0.5, 0.5).setScale(2);
         let button2 = this.add.image(game.config.width/2, button2Pad, 'button').setOrigin(0.5, 0.5).setScale(2);   
         button1.setInteractive();
-        button2.setInteractive();
+        button2.setInteractive();        
         this.add.text(game.config.width/2, button1.y + 2, 'PLAY!', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, button2.y + 2, 'QUIT', menuConfig).setOrigin(0.5);
-        
+
+        // Change Alpha on buttons
+        button1.on('pointerover', () => {
+            button1.alpha = 0.5;
+        });
+        button1.on('pointerout', () => {
+            button1.alpha = 1;
+        });
+        button2.on('pointerover', () => {
+            button2.alpha = 0.5;
+        });
+        button2.on('pointerout', () => {
+            button2.alpha = 1;
+        });
+
+        // Got to Play scene
         button1.on('pointerdown', () => {
             this.scene.start('play');
         });
-
+        // Nuke it
         button2.on('pointerdown', () => {
             game.destroy(true);
         });
