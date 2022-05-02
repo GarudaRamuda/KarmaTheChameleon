cf85e80f5ece8375599ede54524570587ed3fe79
garudaramuda <jjek@ucsc.edu>
2022-05-02 00:02:48 -0700

    Fixed more consistency/casing issues

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -48,8 +48,8 @@ class Load extends Phaser.Scene {
 
         // Load Sounds
         this.load.audio('song_drums', './assets/Music/Menu_Loop.ogg');
-        this.load.audio('song_intro', './assets/Music/Song Intro.ogg');
-        this.load.audio('song_loop', './assets/Music/Song Loop.ogg');
+        this.load.audio('song_intro', './assets/Music/Song_Intro.ogg');
+        this.load.audio('song_loop', './assets/Music/Song_Loop.ogg');
         this.load.audio('song_death', './assets/Music/Song_Death.ogg')
         this.load.audio('sound_birds', './assets/sounds/Sound_Birds.ogg');
         this.load.audio('sound_jungle', './assets/sounds/Sound_Jungle.ogg');
